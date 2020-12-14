87df57da466377de2a324ba51845add468e42bbf
evgenij27 <eeevg27@gmail.com>
2020-12-14 22:02:02 +0200

    use method return type to return proper Publisher

--- src/main/java/intempt/rbac/SecuredServiceAdvice.java
+++ src/main/java/intempt/rbac/SecuredServiceAdvice.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.reactivestreams.Publisher;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,7 @@ import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Component;
 import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -26,12 +28,24 @@ public class SecuredServiceAdvice {
     @Around("preSecuredServicePointcut(preSecuredByName)")
     public Object preSecured(ProceedingJoinPoint pjp, PreSecuredByName preSecuredByName) throws Throwable {
         log.info("Before advice");
+
+        MethodSignature method = (MethodSignature) pjp.getSignature();
+        final Class<?> returnType = method.getReturnType();
+
         Mono<Authentication> toInvoke = ReactiveSecurityContextHolder.getContext()
                 .map(SecurityContext::getAuthentication)
                 .filter(auth -> auth.getName().equalsIgnoreCase("bob"))
                 .switchIfEmpty(Mono.error(new AccessDeniedException("Not Bob!")));
 
-        return toInvoke.flatMap(auth -> SecuredServiceAdvice.<Mono<?>>proceed(pjp));
+        if (Flux.class.isAssignableFrom(returnType)) {
+            return toInvoke.flatMapMany(auth -> SecuredServiceAdvice.<Flux<?>>proceed(pjp));
+        }
+
+        if (Mono.class.isAssignableFrom(returnType)) {
+            return toInvoke.flatMap(auth -> SecuredServiceAdvice.<Mono<?>>proceed(pjp));
+        }
+
+        return toInvoke.flatMapMany(auth -> Flux.from(SecuredServiceAdvice.<Publisher<?>>proceed(pjp)));
     }
 
     private static <T extends Publisher<?>> T proceed(ProceedingJoinPoint pjp) {
